240dbcf2c5797bc6cbcb3bb539eae287aa2a0870
peterlee92 <mhyeok92@gmail.com>
2021-11-29 18:23:12 -0800

    code clean up

--- src/App.js
+++ src/App.js
@@ -3,8 +3,6 @@ import React, { useState } from "react";
 import logo from "./logo.svg";
 
 function App() {
-  const [active, setActive] = useState(false);
-
   return <div className="App"></div>;
 }
 
