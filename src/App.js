03329b99dcdd392ea7237bb9b1e6c9b326592d03
peterlee92 <mhyeok92@gmail.com>
2021-11-29 17:54:50 -0800

    installed react app

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+
+import logo from "./logo.svg";
+
+function App() {
+  const [active, setActive] = useState(false);
+
+  return <div className="App"></div>;
+}
+
+export default App;
